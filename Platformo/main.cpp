135164627db509e28afa69b3bbf44f0e3e52e425
Matvego <matvejslenkin436@gmail.com>
2025-02-04 10:21:07 +0400

    Add files via upload

--- Platformo/main.cpp
+++ Platformo/main.cpp
@@ -38,16 +38,37 @@ void draw()
      }
 
 };
+struct Player
+{
+int x;
+int y;
+int w;
+int h;
+HDC image;
+
+void draw()
+     {
+     txTransparentBlt(txDC(),x,y,w,h,image,0,0,RGB(12,184,220));
+
+     }
+
+};
+
 
 int main()
 {
 txCreateWindow (1350, 700);
-Platforma platforma1 = {-125,600,300,100,txLoadImage("Platforma.bmp")};
+HDC fon = txLoadImage("fon.bmp");
+txBitBlt(txDC(),0,0,1350,700,fon);
+Platforma platforma1 = {-160,550,400,200,txLoadImage("Platforma.bmp")};
 platforma1.draw();
-
+Player player1 = {50,450,200,140,txLoadImage("TestChelik3.bmp")};
+player1.draw();
 
 txTextCursor (false);
 txDeleteDC(platforma1.image);
+txDeleteDC(player1.image);
+txDeleteDC(fon);
 return 0;
 }
 
